b2506e28b3a86a8c932ffda9e6fca138a12bec62
MandyBizinbox <mandy@bizinbox.co.za>
2026-07-09 14:23:57 +0200

    Load builder V2 runtime globally

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "@/App.css";
 import "@/index.css";
+import "./components/product-builder/productBuilderV2Runtime";
 import { BrowserRouter, Routes, Route, Navigate, useLocation } from "react-router-dom";
 import { Toaster } from "sonner";
 import { AuthProvider, useAuth } from "./context/AuthContext";
