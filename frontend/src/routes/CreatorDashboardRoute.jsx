dcc22901275d8c1864a0b05930c05fb0a006e0dd
MandyBizinbox <mandy@bizinbox.co.za>
2026-07-17 23:56:42 +0200

    Defer Creator Studio runtimes to creator routes

---
+++ frontend/src/routes/CreatorDashboardRoute.jsx
@@ -0,0 +1,5 @@
+import "../components/product-builder/productBuilderV2Runtime";
+import "../components/product-builder/productBuilderTextColourRuntime";
+import "../components/product-builder/productBuilderDraftButtonRuntime";
+
+export { default } from "../pages/BandDashboard";
