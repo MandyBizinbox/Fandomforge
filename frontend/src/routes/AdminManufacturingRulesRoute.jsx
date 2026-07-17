6acbef72835d0a0e9fa1463977da73b90620cdac
MandyBizinbox <mandy@bizinbox.co.za>
2026-07-17 23:56:59 +0200

    Defer manufacturing runtimes to manufacturing routes

---
+++ frontend/src/routes/AdminManufacturingRulesRoute.jsx
@@ -0,0 +1,4 @@
+import "../components/admin/adminManufacturingRulesThemeRuntime";
+import "../components/admin/legacyPrintOptionCostingSeedRuntime";
+
+export { default } from "../pages/admin/AdminManufacturingRules";
