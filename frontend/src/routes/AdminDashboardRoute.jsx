68395239fba05bf3bd5c65c7c1f2f7997d1c73f6
MandyBizinbox <mandy@bizinbox.co.za>
2026-07-17 23:56:51 +0200

    Defer admin and builder runtimes to admin routes

---
+++ frontend/src/routes/AdminDashboardRoute.jsx
@@ -0,0 +1,7 @@
+import "../components/product-builder/productBuilderV2Runtime";
+import "../components/product-builder/productBuilderTextColourRuntime";
+import "../components/product-builder/productBuilderDraftButtonRuntime";
+import "../components/admin/adminManufacturingRulesThemeRuntime";
+import "../components/admin/legacyPrintOptionCostingSeedRuntime";
+
+export { default } from "../pages/AdminDashboard";
